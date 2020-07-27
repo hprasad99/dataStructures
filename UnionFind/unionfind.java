61a15384a40a79c3e61f3b81b6d3683cb674930b
Himanshu Prasad <chatwith.himanshu27@gmail.com>
2020-07-27 18:49:24 +0530

    unionfind v_0.1

---
+++ UnionFind/unionfind.java
@@ -0,0 +1,83 @@
+public class unionfind{
+    //The number of elements in this union find
+    private int size;
+
+    //Used to track the size of each of the component
+    private int[] sz;
+
+    private int[] id;
+
+    private int numComponents;
+
+    public unionfind(int size){
+        if(size<=0) throw new IllegalArgumentException("Size <= 0 is not allowed");
+
+        this.size = numComponents = size;
+        sz = new int[size];
+        id = new int[size];
+    
+        for(int i=0;i<size;i++){
+            id[i] = i;  // Link to itself (self root)
+            sz[i] = 1;  // Each component is originally of size 1
+        }
+    }
+
+    //Find which component /set 'p' belongs to, takes amortized constant time
+
+    public int find(int p){
+        
+        //Find the root of the component/set
+        int root = p;
+        while(root!=id[root]) root = id[root];
+
+        //Compress the path leading back to the root
+        //Doing this operation is called "path compression"
+        //and is what gives us amortized time complextiy
+        while(p!=root){
+            int next = id[p];
+            id[p] = root;
+            p = next;
+        }
+
+        return root;
+    }
+
+    public boolean connected(int p,int q){
+        return find(p) == find(q);
+    }
+
+    public int componentSize(int p){
+        return sz[find(p)];
+    }
+
+    //Return the number of elements in this UnionFind/Disjoint set
+    public int size(){
+        return size;
+    }
+
+    //Returns the number of remaining components/sets
+    public int components(){
+        return numComponents;
+    }
+
+    //Unify the components/sets containing elements 'p' and 'q'
+    public void unify(int p,int q){
+        int root1 = find(p);
+        int root2 = find(q);
+
+        //These elements are already in the same group
+        if(root1 == root2) return;
+
+        //Merge smaller component/set into the larger one.
+        if(sz[root1]<sz[root2]){
+            sz[root2]+=sz[root1];
+            id[root1]= root2;
+        }else{
+            sz[root1]+=sz[root2];
+            id[root2]=root1;
+        }
+        //Since the roots found are different we know that the number of components/sets has decreased by one
+        numComponents--;
+    }
+
+}
