2e0fed50c5489ff92cd8a690a43f436dfe2f8745
Himanshu Prasad <chatwith.himanshu27@gmail.com>
2020-08-09 19:56:29 +0530

    Child Class

---
+++ HashTable/HashTableQuadraticProbing.java
@@ -0,0 +1,44 @@
+public class HashTableQuadraticProbing<K, V> extends HashTableOpenAddressingBase<K, V> {
+
+    public HashTableQuadraticProbing() {
+        super();
+    }
+
+    public HashTableQuadraticProbing(int capacity) {
+        super(capacity);
+    }
+
+    public HashTableQuadraticProbing(int capacity, double loadFactor) {
+        super(capacity, loadFactor);
+    }
+
+    // Given a number this method finds the next
+    // power of two above this value
+    private static int nextPowerOfTwo(int n) {
+        return Integer.highestOneBit(n) << 1;
+    }
+
+    //No setup required for quadratic probing
+    @Override
+    protected void setupProbing(K key) {
+    }
+
+    @Override
+    protected int probe(int x) {
+        // Quadratic probing function (x^2+x)/2
+        return (x * x + x) >> 1;
+    }
+
+    //Adjust the capacity of the hashtable to be a power of two.
+    @Override
+    protected void increaseCapacity() {
+        capacity = nextPowerOfTwo(capacity);
+    }
+
+    @Override
+    protected void adjustCapacity() {
+        int pow2 = Integer.highestOneBit(capacity);
+        if(capacity == pow2) return;
+        increaseCapacity();
+    }
+}
